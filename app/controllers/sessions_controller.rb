7ba9d2b91bc8d35b6b0e485e7738889d91701e3d
EC2 Default User <ec2-user@ip-172-31-5-37.us-east-2.compute.internal>
2018-03-11 05:39:01 +0000

    follow, unfollow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
   def create
     email = params[:session][:email].downcase
     password = params[:session][:password]
-    if login(email, password)
-      flash[:success] = "ログインに成功しました。"
-      redirect_to @user
-    else
-      flash.now[:danger] = "ログインに失敗しました。"
-      render "new"
-    end
+      if login(email, password)
+        flash[:success] = "ログインに成功しました。"
+        redirect_to root_url
+      else
+        flash.now[:danger] = "ログインに失敗しました。"
+        render "new"
+      end
   end
 
   def destroy
